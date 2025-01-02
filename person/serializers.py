22cdf8dcbce0228681216ba68ace136847e188cb
Johannes Jooste <johan@jooste.org>
2025-01-02 10:47:39 +0200

    Update profile.serializers.py
    To include father, mother, full and half-sibling unique_ids.

--- person/serializers.py
+++ person/serializers.py
@@ -1,7 +1,6 @@
 from rest_framework import serializers
 from .models import Profile, Marriage
 from location.models import Location
-from datetime import date
 
 class LocationSerializer(serializers.ModelSerializer):
     class Meta:
@@ -9,11 +8,21 @@ class LocationSerializer(serializers.ModelSerializer):
         fields = '__all__'
 
 class ProfileSerializer(serializers.ModelSerializer):
+    father_unique_id = serializers.CharField(source="father.unique_id", read_only=True)
+    mother_unique_id = serializers.CharField(source="mother.unique_id", read_only=True)
+    full_siblings_unique_id = serializers.SerializerMethodField()
+    half_siblings_unique_id = serializers.SerializerMethodField()
     location = LocationSerializer(read_only=True)
     
     class Meta:
         model = Profile
         fields = '__all__'
+        
+    def get_full_siblings_unique_id(self, obj):
+        return list(obj.get_full_siblings().values_list("unique_id", flat=True))
+
+    def get_half_siblings_unique_id(self, obj):
+        return list(obj.get_half_siblings().values_list("unique_id", flat=True))
         
 class MarriageSerializer(serializers.ModelSerializer):
     location = LocationSerializer(read_only=True)
